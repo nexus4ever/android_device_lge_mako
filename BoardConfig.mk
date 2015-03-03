07c791be55ce94169e2a83b727708ec55b4862a5
Lars Greiss <kufikugel@googlemail.com>
2015-03-03 11:05:55 +0000

    Mako: disable touch boost debug spam we use our own kernel implementation but still want to keep up compatibility to third party kernels which may use stock mpdecision.
    Therefore we simply disable debug for it so that we do not have complains
    about missing stock mpdecision.
    
    Change-Id: I95a0f8b4e20f08f4334f256fc746efbc1e69abf3
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,8 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677 user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
+TOUCH_BOOST_DEBUG := false
+
 # Inline kernel building
 TARGET_KERNEL_CONFIG := mako_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/mako
