d253ab8519332257c591efc6bef3792903c4fd88
spezi77 <spezi77@gmx.com>
2016-03-01 14:10:36 +0100

    PowerHAL: touch_boost: kill logcat spam  * Let's just get rid of this annoying error message as there is currently no hope for a proper solution.  * PowerHAL: touch_boost: failed to send: No such file or directory
    Change-Id: Ib8c8ef0403d4da7ac6512f461a3ed6dd0fa07982
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- power/power_mako.c
+++ power/power_mako.c
@@ -314,7 +314,7 @@ static void touch_boost()
     rc = sendto(client_sockfd, data, strlen(data), 0,
         (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
     if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
+        //ALOGE("%s: failed to send: %s", __func__, strerror(errno));
     }
 }
 
