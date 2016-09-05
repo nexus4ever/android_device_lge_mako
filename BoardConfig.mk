0d2b59e886c798c22e02792a76b86844ab094e5f
spezi77 <spezi77@gmx.com>
2016-09-05 20:58:55 +0200

    Inline kernel building instead prebuilt kernel.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,13 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=mako lpj=67677 user_debug=31
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
+# Inline kernel building
+TARGET_KERNEL_CONFIG := revival_mako_defconfig
+TARGET_KERNEL_SOURCE := kernel/lge/mako
+TARGET_GCC_VERSION_ARM := 5.4.1
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+KERNEL_TOOLCHAIN := /home/spezi77/android/uber-tc/arm-eabi-5.x/bin
+
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
 BOARD_USES_FLUENCE_INCALL := true
--- device.mk
+++ device.mk
@@ -41,16 +41,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     charger_res_images
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-ifeq ($(USE_SVELTE_KERNEL),true)
-LOCAL_KERNEL := device/lge/mako_svelte-kernel/kernel
-else
-LOCAL_KERNEL := device/lge/mako-kernel/kernel
-endif
-else
-LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 PRODUCT_COPY_FILES := \
 	$(LOCAL_KERNEL):kernel
 
