9863b959bed0e40a3af978d8b6a5a652ce34ee4c
spezi77 <spezi77@gmx.com>
2016-11-13 09:28:10 +0100

    Revert "mako: fix Camera HAL v1 (kldoc)."
    This reverts commit 03804dd6cebf40d30a5e87bd0c06f86735d90a3c.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,7 +128,6 @@ USE_MINIKIN := true
 
 # Camera
 TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS := true
-TARGET_HAS_CAMERA_HAL_V1 := true
 TARGET_USES_MEDIA_EXTENSIONS := true
 
 # Include an expanded selection of fonts
